4581bda34d9707c59e1301afe18b27f7d4f64365
stephaniey2022 <56015168+stephaniey2022@users.noreply.github.com>
2020-04-23 01:28:24 -0500

    Slice 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
+import React, { useState,useEffect } from 'react';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -29,12 +29,27 @@ const schedule = {
   ]
 };
 
-const App = () => (
-  <Container>
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </Container>
-);
+const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url)
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, [])
+
+  return(
+    <Container>
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </Container>
+  );
+};
 
 const Banner = ({ title }) => (
   <Title>{ title }</Title>
